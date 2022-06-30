cb52200d862a7394843df6040f035b252c822a66
HaiFazwin <108342947+HaiFazwin@users.noreply.github.com>
2022-06-30 23:08:17 +0800

    Update influenza.py

--- influenza.py
+++ influenza.py
@@ -11,13 +11,13 @@ st.sidebar.header('How are you?')
 
 def user_input_features():
     Temperature = st.sidebar.slider('Your Temperature', 35, 36, 37,38,39,40,41,42,43,44)
-    RunningNose = st.sidebar.slider('Running Nose?', 1, 0)
-    Cough = st.sidebar.slider('Coughing?',1, 0)
-    Dizziness = st.sidebar.slider('Dizziness?', 1, 0)
+    RunningNose = st.sidebar.slider('Running Nose', 1, 0)
+    Cough = st.sidebar.slider('Coughing',1, 0)
+    Dizziness = st.sidebar.slider('Dizziness', 1, 0)
     data = {'Temperature': Your Temperature,
-            'RunningNose': Running Nose?,
-            'Cough': Coughing?,
-            'Dizziness': Dizziness?}
+            'RunningNose': Running Nose,
+            'Cough': Coughing,
+            'Dizziness': Dizziness}
     features = pd.DataFrame(data, index=[0])
     return features
 
@@ -26,7 +26,7 @@ df = user_input_features()
 st.subheader('User Input parameters')
 st.write(df)
 
-iris = pd.read_csv('https://raw.githubusercontent.com/HaiFazwin/stuntedkid/main/Influenza%20-%20Sheet1%20(1).csv')
+iris = pd.read_csv('https://raw.githubusercontent.com/HaiFazwin/stuntedkid/main/Influenza%20-%20Sheet1%20(2).csv')
 X = iris.drop('Result',axis=1)
 Y = iris.Result
 
